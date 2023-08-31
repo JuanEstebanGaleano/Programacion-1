a26625a7960811a4bc71006ad12da0576d48baf1
El viejo firu <juane.galeanob@uqvirtual.edu.co>
2023-08-31 18:26:02 -0500

    commit

--- Ejercicios/CuentaBancaria.java
+++ Ejercicios/CuentaBancaria.java
@@ -1,27 +1,20 @@
-public class CuentaBancaria {
-    String titularCuenta;
-    double balance;
-
-    // Constructor
-    CuentaBancaria(String titularCuenta, double balance) {
-        this.titularCuenta = titularCuenta;
-        this.balance = balance;
+class CuentaBancaria{
+    String titular;
+    Double balance;
+    CuentaBancaria(String titular, Double balance){
+        this.titular= titular;
+        this.balance=balance;
     }
-    // Método para depositar dinero en la cuenta
-     public void  Depositar(double cantidad) {
-        balance += cantidad;
+    public void Depositar(double cantidad){
+    balance+=cantidad;
     }
-    // Método para retirar dinero de la cuenta
-    public void Retirar(double cantidad) {
-        if (cantidad <= balance) {
-            balance -= cantidad;
-        } else {
-            System.out.println("No hay suficiente saldo en la cuenta.");
+    public void retirar(double cantidad){
+        if(cantidad<=balance){
+        balance-=cantidad;
+        }else{
+            System.out.println("No tiene suficiente saldo");
         }
     }
-
-    // Método para obtener el balance actual de la cuenta
-   public double obtenerBalance() {
-        return balance;
+    public static void main(String[] args) {
     }
 }
