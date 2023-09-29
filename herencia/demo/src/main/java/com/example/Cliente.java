151fca7e45b468fb6f667209fb4b9e7fdb6d40de
El viejo firu <juane.galeanob@uqvirtual.edu.co>
2023-09-28 19:37:02 -0500

    commit

--- herencia/demo/src/main/java/com/example/Cliente.java
+++ herencia/demo/src/main/java/com/example/Cliente.java
@@ -2,5 +2,11 @@ package herencia.demo.src.main.java.com.example;
 import java.util.Date;
 public class Cliente extends Persona{
     private int idCliente;
-    private date fecha
+    private Date fechaRegistro;
+    private boolean vip;
+    private static int ContadorClientes;
+
+    public Cliente(Date fechaRegistro, boolean vip){
+        this.idCliente=++ContadorClientes;
+    }
 }
